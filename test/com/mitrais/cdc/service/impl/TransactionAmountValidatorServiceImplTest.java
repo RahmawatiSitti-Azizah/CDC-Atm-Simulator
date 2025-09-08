f7a664dcef1aaab3b253aae40ac1ef06233aa895
MITRAIS\Rahmawati_SA294 <rahmawatisitti.azizah@mitrais.com>
2025-09-08 16:15:43 +0700

    Fix issue on unit test service impl layer

--- test/com/mitrais/cdc/service/impl/TransactionAmountValidatorServiceImplTest.java
+++ test/com/mitrais/cdc/service/impl/TransactionAmountValidatorServiceImplTest.java
@@ -20,31 +20,31 @@ public class TransactionAmountValidatorServiceImplTest {
     @Test
     public void testValidateWithdrawAmount_whenAmountValid_thenNoExceptionThrow() throws Exception {
         Money money = mock(Money.class);
-        when(money.isMultipleOf(any())).thenReturn(true);
-        when(money.isMoreThan(any())).thenReturn(true);
+        when(money.isMultipleOf(anyInt())).thenReturn(true);
+        when(money.isMoreThan(any())).thenReturn(false);
         serviceInTest.validateWithdrawAmount(money);
-        verify(money, times(1)).isMultipleOf(any());
+        verify(money, times(1)).isMultipleOf(anyInt());
         verify(money, times(1)).isMoreThan(any());
     }
 
     @Test
     public void testValidateWithdrawAmount_whenAmountNotMultipleOfTen_thenThrowInvalidAmountException() throws Exception {
         Money money = mock(Money.class);
-        when(money.isMultipleOf(any())).thenReturn(false);
+        when(money.isMultipleOf(anyInt())).thenReturn(false);
         Exception exception = Assert.assertThrows(Exception.class, () -> serviceInTest.validateWithdrawAmount(money));
         Assert.assertEquals(ErrorConstant.INVALID_AMOUNT, exception.getMessage());
-        verify(money, times(1)).isMultipleOf(any());
+        verify(money, times(1)).isMultipleOf(anyInt());
         verify(money, never()).isMoreThan(any());
     }
 
     @Test
     public void testValidateWithdrawAmount_whenAmountExceed1000_thenThrowMaximumExceedException() throws Exception {
         Money money = mock(Money.class);
-        when(money.isMultipleOf(any())).thenReturn(true);
-        when(money.isMoreThan(any())).thenReturn(false);
+        when(money.isMultipleOf(anyInt())).thenReturn(true);
+        when(money.isMoreThan(any())).thenReturn(true);
         Exception exception = Assert.assertThrows(Exception.class, () -> serviceInTest.validateWithdrawAmount(money));
         Assert.assertEquals(ErrorConstant.MAXIMUM_TRANSACTION_AMOUNT_EXCEED, exception.getMessage());
-        verify(money, times(1)).isMultipleOf(any());
+        verify(money, times(1)).isMultipleOf(anyInt());
         verify(money, times(1)).isMoreThan(any());
     }
 }
